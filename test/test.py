c5592f022404b1987948f1635d0965848879cfe9
kimmseojin <gini09261234@gmail.com>
2020-07-23 20:19:44 +0900

    better program with pygame

--- test/test.py
+++ test/test.py
@@ -1,1 +1,10 @@
-print("hello")
+import pygame
+
+
+
+win = pygame.display.set_mode((500,500))
+
+play = True
+while play:
+    pygame.draw.circle(win, (0,255,255), (200, 300), 100, 3)
+    pygame.display.flip()
